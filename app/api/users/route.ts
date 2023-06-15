0d68dc2755e061a2b765256c8b249d4840fa76dd
Anne Jacobson <mail@ihoey.com>
2023-06-16 00:02:21 +0800

    Update route.ts

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,15 +1,11 @@
 import prisma from "@/lib/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
-import { hash } from "bcrypt";
+
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
-  
-    const user = await prisma.user.create({
-      data: {
-        email,
-        password: await hash(password, 10),
-      },
-    });
-    return NextResponse.json(user);
+
+  const users = await prisma.users.findMany();
+
+  res.status(200).json(users);
 }
