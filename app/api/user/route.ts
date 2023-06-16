891c2dff4f9727d8dee933e46a8b9b13fe833291
ihoey <mail@ihoey.com>
2023-06-16 10:04:37 +0800

    fix: "NextApiRequest" is not a valid type for the function first argument.

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,8 +1,7 @@
 import prisma from "@/lib/prisma";
-import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: Request, res: Response) {
   const users = await prisma.users.findMany();
 
   return NextResponse.json(users);
